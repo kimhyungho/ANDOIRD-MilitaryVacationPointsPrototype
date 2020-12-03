ad36abda18ebf7a7f71a1fd98ec072c3bcfc0bff
kimhyungho <rla88175@gmail.com>
2020-12-03 18:22:02 +0900

    1203

--- app/src/main/java/com/appdev/mvp/SettingsFragment.kt
+++ app/src/main/java/com/appdev/mvp/SettingsFragment.kt
@@ -1,10 +1,15 @@
 package com.appdev.mvp
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import org.w3c.dom.Text
@@ -13,6 +18,9 @@ class SettingsFragment : Fragment() {
     lateinit var nameView: TextView
     lateinit var departmentView: TextView
     lateinit var remainDayView: TextView
+    lateinit var uploadImageView: ImageView
+
+    lateinit var filePath: String
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +39,7 @@ class SettingsFragment : Fragment() {
         nameView = view.findViewById(R.id.st_name)
         departmentView = view.findViewById(R.id.st_department)
         remainDayView = view.findViewById(R.id.st_remain_day)
+        uploadImageView = view.findViewById(R.id.setting_upload_image)
     }
 
     private fun initListener() {
@@ -41,7 +50,35 @@ class SettingsFragment : Fragment() {
         nameView.text = name
         departmentView.text = department
         remainDayView.text = remainDay.toString() + "일"
+
+
+        uploadImageView.setOnClickListener {
+            getPicture()
+        }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1000) {
+            val uri: Uri = data!!.data!!
+            filePath = getImageFilePath(uri)
+        }
+    }
 
+    fun getImageFilePath(contentUri: Uri): String {
+        var columnIndex = 0                                          // 절대 경로를 얻기 위한 과정
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = activity!!.contentResolver.query(contentUri, projection, null, null, null)
+        if (cursor!!.moveToFirst()) {
+            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        }
+        return cursor.getString(columnIndex)
+    }
+
+    private fun getPicture() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        intent.type = "image/*"
+        startActivityForResult(intent, 1000)
+    }
 }

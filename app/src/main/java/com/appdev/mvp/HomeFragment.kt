b1356f14f0e2b4214a90d07cb9771348d9cccd49
kimhyungho <rla88175@gmail.com>
2020-12-05 14:29:42 +0900

    1205

--- app/src/main/java/com/appdev/mvp/HomeFragment.kt
+++ app/src/main/java/com/appdev/mvp/HomeFragment.kt
@@ -17,7 +17,6 @@ import java.util.*
 
 class HomeFragment : Fragment() {
 
-    lateinit var calendarView: MaterialCalendarView
     lateinit var searchView: ImageView
     lateinit var todayBtn: TextView
     lateinit var searchBtn: ImageView
@@ -50,20 +49,17 @@ class HomeFragment : Fragment() {
     }
 
     private fun initView(view: View) {
-        calendarView = view.findViewById<MaterialCalendarView>(R.id.calendar)
+//        calendarView = view.findViewById<MaterialCalendarView>(R.id.calendar)
         searchView = view.findViewById<ImageView>(R.id.home_search)
         todayBtn = view.findViewById(R.id.home_today)
         searchBtn = view.findViewById(R.id.home_search)
 
-        calendarView
 
 
     }
 
     private fun initListener() {
-        todayBtn.setOnClickListener {
-            calendarView.setCurrentDate(calendarView.currentDate, true)
-        }
+
 
 
 
@@ -74,9 +70,6 @@ class HomeFragment : Fragment() {
 //        }
 
 
-        calendarView.showOtherDates = MaterialCalendarView.SHOW_ALL
-        setEvent(pinkDateList, green)
-        setEvent(grayDateList, gray)
 
         searchView.setOnClickListener {
             val bottomSheet = SearchFragment()
@@ -85,84 +78,6 @@ class HomeFragment : Fragment() {
 
     }
 
-    private fun setEvent(dateList: MutableList<String>, color: Int) {
-        var calendarDayList: MutableList<CalendarDay> = ArrayList()
-        for (string in dateList) {
-            var calendar: CalendarDay? = getCalendarDay(string)
-            if (calendar != null) {
-                calendarDayList.add(calendar)
-            }
-        }
-
-        val datesLeft: MutableList<CalendarDay> = ArrayList()
-        val datesCenter: MutableList<CalendarDay> = ArrayList()
-        val datesRight: MutableList<CalendarDay> = ArrayList()
-        val datesIndependent: MutableList<CalendarDay> = ArrayList()
-
-        for (calendarDay in calendarDayList) {
-            var right = false
-            var left = false
-
-            for (day1 in calendarDayList) {
-                if (calendarDay == CalendarDay.from(day1.year, day1.month, day1.day + 1)) {
-                    left = true
-                }
-
-                if (day1 == CalendarDay.from(
-                        calendarDay.year,
-                        calendarDay.month,
-                        calendarDay.day + 1
-                    )
-                ) {
-                    right = true
-                }
-            }
-            if (left && right) {
-                datesCenter.add(calendarDay)
-            } else if (left) {
-                datesLeft.add(calendarDay)
-            } else if (right) {
-                datesRight.add(calendarDay)
-            } else {
-                datesIndependent.add(calendarDay)
-            }
-        }
-
-        if (color == green) {
-            setDecor(datesCenter, R.drawable.m_center);
-            setDecor(datesLeft, R.drawable.m_left);
-            setDecor(datesRight, R.drawable.m_right);
-            setDecor(datesIndependent, R.drawable.m_independent);
-        } else {
-            setDecor(datesCenter, R.drawable.g_center)
-            setDecor(datesLeft, R.drawable.g_left)
-            setDecor(datesRight, R.drawable.g_right)
-            setDecor(datesIndependent, R.drawable.g_independent)
-
-        }
-    }
-
-    private fun setDecor(calendarDayList: List<CalendarDay>, drawable: Int) {
-        calendarView.addDecorator(EventDecorator(activity!!, drawable, calendarDayList))
-    }
-
-    private fun getCalendarDay(date: String): CalendarDay? {
-        val sdf = SimpleDateFormat(DATE_FORMAT)
-        try {
-            val input = sdf.parse(date)
-            val cal = Calendar.getInstance()
-            cal.time = input
-            return CalendarDay.from(
-                cal.get(Calendar.YEAR),
-                cal.get(Calendar.MONTH) + 1,
-                cal.get(Calendar.DAY_OF_MONTH)
-            )
-        } catch (e: NullPointerException) {
-            return null
-        } catch (e: ParseException) {
-            return null
-        }
-    }
 
 
 }
